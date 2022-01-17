10d8eafd6c72526e9c6d57a57e86580a3bdd45ab
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-17 17:19:06 +0100

    ossatura seconda section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,19 @@
 <template>
     <main>
         <Jumbotron/>
+        <ServiceSection/>
     </main>
 </template>
 
 <script>
 import Jumbotron from "./Jumbotron.vue";
+import ServiceSection from "./ServiceSection.vue";
 
 export default {
   name: "Main",
   components:{
       Jumbotron,
+      ServiceSection,
   }
 };
 </script>
---
+++ src/components/ServiceSection.vue
@@ -0,0 +1,78 @@
+<template>
+    <section>
+        <div class="container">
+            <div class="first-info">
+                <div class="info">
+                    <h4>OUR BUSINESS AREAS</h4>
+                    <h2>Excellence in Services</h2>
+                    <p>We are leaders in providing consultancy services with a set of cutting-edge technologies and a team of experienced and renowed professionals. These are some options that you can hire</p>
+                </div>
+                <div class="btn">
+                    SEE ALL
+                </div>
+            </div>
+            <div class="box-section">
+                <!-- da popoplare con un array? -->
+                <div class="single-box">
+
+                </div>
+                <div class="single-box">
+
+                </div>
+                <div class="single-box">
+
+                </div>
+                <div class="single-box">
+
+                </div>
+                <div class="single-box">
+
+                </div>
+                <div class="single-box">
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+  name: "ServiceSection",
+  
+};
+</script>
+
+
+<style scoped lang="scss">
+section{
+    height: 800px;
+    background-color: #eef4ed;
+    padding:55px 0;
+    .container{
+        width: 70%;
+        margin: auto;
+        .first-info{
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            align-items: flex-end;
+            .info{
+                width: 70%;
+                padding: 6px;
+            }
+        }
+        .box-section{
+            display: flex;
+            flex-wrap: wrap;
+            .single-box{
+                width: calc((100% / 3) - 40px);
+                // test
+                height: 250px;
+                background-color: pink;
+                margin: 20px;
+            }
+        }       
+    }
+}
+</style>

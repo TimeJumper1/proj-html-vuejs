2dd0042cce093530895b93a9004ce2c503633bb4
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-18 18:09:39 +0100

    actions section completata no javascript

--- src/components/ActionSection.vue
+++ src/components/ActionSection.vue
@@ -2,42 +2,42 @@
     <section>
         <div class="top-info">
             <h5>WE DO MORE FOR EVERYONE</h5>
-            <h2>Action & projects</h2>
+            <h2>Action & <span class="decor">Projects</span></h2>
             <ul>
-                <li>all</li>
-                <li>all</li>
-                <li>all</li>
-                <li>all</li>
-                <li>all</li>
-                <li>all</li>
-                <li>all</li>
+                <li>ALL</li>
+                <li>INSTITUTIONAL</li>
+                <li>SOCIAL</li>
+                <li>EVENTS</li>
+                <li>INNOVATION</li>
+                <li>ENVIRONMENT</li>
+                <li>TECHNOLOGY</li>
             </ul>
         </div>
         <div class="foto-section">
             <!-- da popolarecon un v-for -->
             <div class="foto-card">
                 <img src="../assets/img/project-1.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>Academic professional program in social media</h3>
             </div>
             <div class="foto-card">
                 <img src="../assets/img/project-2.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>President's speech at the annual meeting</h3>
             </div>
             <div class="foto-card">
                 <img src="../assets/img/project-3.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>International business trip in Shanghai</h3>
             </div>
             <div class="foto-card">
                 <img src="../assets/img/project-4.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>Technology workshop with education theme</h3>
             </div>
             <div class="foto-card">
                 <img src="../assets/img/project-5.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>Donation of clothes and food to the partner NGO</h3>
             </div>
             <div class="foto-card">
                 <img src="../assets/img/project-6.jpg" alt="mani sul computer">
-                <h3>testo per vedere se tutto va</h3>
+                <h3>Confraternization of the procurement team</h3>
             </div>
 
         </div>
@@ -57,12 +57,33 @@ section{
     margin: 100px 0;
     .top-info{
         text-align: center;
+        h5{
+            font-size: 10px;
+            color:#1ba8a7 ;
+        }
+        h2{
+            font-size: 33px;
+            margin: 30px;
+            .decor{
+                color: #058283;
+                background-color: #daeced;
+                padding: 7px 17px;
+                border-radius: 4px;
+            }
+        }
         ul{
             list-style-type: none;
             display: flex;
             justify-content: center;
+            margin: 8px 20px;
             li{
-                margin: 3px 10px;
+                
+                padding: 6px 14px;
+                color: #66727a;
+                &:hover{
+                    background-color: #daeced;
+                    transition: 0.3s;
+                }
             }
         }
     }
@@ -92,6 +113,8 @@ section{
                 left: 50%;
                 top: 70%;
                 transform: translate(-50% , -30%);
+                width: 80%;
+                font-size: 22px;
             }
         }
 

1818bdf030ccaba0ba2d6521082a665da4a7dc20
TimeJumper1 <benassai.stefano.1@gmail.com>
2022-01-18 17:55:35 +0100

    company section completata

--- src/components/CompanySection.vue
+++ src/components/CompanySection.vue
@@ -3,26 +3,32 @@
     <div class="col sinistra">
       <div class="top-info">
         <h4>ABOUT THE NETWORK</h4>
-        <h2>The Company</h2>
+        <h2><span class="decor">The</span> Company</h2>
         <p>for 12 years we have been providing audit and warranty, financial advice, risk advice, taxes and related services to select clients</p>
       </div>
       <div class="info-container">
        
         <div class="info-card">
-
+          <h2><i class="fas fa-ribbon"></i>Tradition </h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div class="info-card">
-
+          <h2><i class="fas fa-lock"></i>Security </h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div class="info-card">
-
+          <h2><i class="far fa-edit"></i>Certificate </h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div class="info-card">
-
+          <h2><i class="fas fa-graduation-cap"></i>Expertise</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
+        
+        
       </div>
-      <div>
-        <span class="btn">bottone</span><span class="btn">bottone</span>
+      <div >
+        <span class="btn">GET IN TOUCH</span><span class="btn">READ MORE</span>
       </div>
     </div>
     <div class="col destra">
@@ -57,6 +63,35 @@ section{
   background-color: black;
   .col{
     width: 50%;
+    .btn{
+        margin: 20px 10px;
+        padding: 7px 17px;
+        border: 1px solid #058283 ;
+        cursor: pointer;
+        &:hover{
+          background-color: #058283 ;
+          transition: 0.3s;
+        }
+      }
+    .top-info{
+      h4{
+        font-size: 10px;
+        color:#039293 ;
+      }
+      h2{
+        font-size: 40px;
+        margin: 17px 0;
+        .decor{
+          background-color:#0e272d ;
+          padding: 4px 9px;
+        }
+      }
+      p{
+        color: #888a94;
+        font-size: 14px;
+        margin: 17px 0;
+      }
+    }
     .image{
       width: 100%;
       max-height: 100%;
@@ -68,9 +103,22 @@ section{
       .info-card{
         width: calc((100% / 2) - 20px);
         margin: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
         // test
-        background-color: pink;
+       
         height: 150px;
+        h2{
+          font-size: 19px;
+          padding: 10px 0;
+          i{
+            color:#039293  ;
+          }
+        }
+        p{
+          color: #888a94;
+        }
       }
     }
   }
@@ -93,7 +141,7 @@ section{
     top: -35px;
     left: 50%;
   }
-  .bullets-decorations{
+  .bottom{
     transform: translate(-50% , 0);
     bottom: -35px;
     left: 50%;
